24e9df49946a27178249755667e3e41991ffd134
Vladimir Sterlin <vovasterlin@yahoo.com>
2021-10-28 22:03:24 -0700

    jwt generation for access token

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/go-chi/chi v1.5.4
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 )
--- internal/user/token.go
+++ internal/user/token.go
@@ -1,6 +1,11 @@
 package user
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+)
 
 func newCookie(n string, v string) *http.Cookie {
 	return &http.Cookie{
@@ -11,18 +16,34 @@ func newCookie(n string, v string) *http.Cookie {
 	}
 }
 
-func generateAccesToken(u *User) *http.Cookie {
-	//TODO
-	return newCookie("access_token", u.FirstName+u.Username+"access_token")
+func newClaims(u *User) *jwt.MapClaims {
+	return &jwt.MapClaims{
+		"id":       u.Id,
+		"username": u.Username,
+		"email":    u.Email,
+	}
 }
 
-func generateRefreshToken(u *User) *http.Cookie {
+func generateAccesTokenCookie(u *User) *http.Cookie {
+	//TODO
+
+	atSecret := []byte("abcdefg")
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, newClaims(u))
+	atStr, err := at.SignedString(atSecret)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return newCookie("access_token", atStr)
+}
+
+func generateRefreshTokenCookie(u *User) *http.Cookie {
 	//TODO
 	return newCookie("refresh_token", u.FirstName+u.Username+"refresh_token")
 }
 
 func SetTokenCookies(w http.ResponseWriter, u *User) {
-	at, rt := generateAccesToken(u), generateRefreshToken(u)
+	at, rt := generateAccesTokenCookie(u), generateRefreshTokenCookie(u)
 	http.SetCookie(w, at)
 	http.SetCookie(w, rt)
 }
