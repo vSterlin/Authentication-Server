c6ab4e3a312bba33ac4bc5452994f73ea787109c
Vladimir Sterlin <vovasterlin@yahoo.com>
2021-10-30 13:39:30 -0700

    rm unnecessary if statement

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -10,10 +10,8 @@ func IsAuthenticated(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		if u := r.Context().Value(user.UserContext).(*user.User); u == nil {
-			if u == nil {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
-			}
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
 		}
 
 		next.ServeHTTP(w, r)
