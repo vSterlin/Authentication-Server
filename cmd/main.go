3ddb5f74793ed6e9608f47e1b84a5a5817d3859c
Vladimir Sterlin <vovasterlin@yahoo.com>
2021-11-01 00:05:10 -0700

    separate package for configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	"fmt"
+
+	"github.com/vSterlin/auth/internal/config"
 	"github.com/vSterlin/auth/internal/server"
 )
 
 func main() {
-	godotenv.Load()
+	if err := config.Load(); err != nil {
+		fmt.Println(err.Error())
+	}
 	s := server.NewServer(8080)
 	s.Init()
 	defer s.Shutdown()
